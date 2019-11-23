4d9b536a23809ee3b0ea1f9f481371788ee6d9fb
Sahil Jain <sjai013.ece@gmail.com>
2019-11-23 15:05:47 +1000

    improvements to hotbar logic

--- Assets/Scripts/AbilityHotbarManager.cs
+++ Assets/Scripts/AbilityHotbarManager.cs
@@ -1,16 +1,36 @@
+/*
+ * Created on Sat Nov 23 2019
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2019 Sahil Jain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
-using Unity.Collections;
 using MyGameplayAbilitySystem.AbilitySystem.MonoBehaviours;
 using GameplayAbilitySystem.AbilitySystem.Components;
 using GameplayAbilitySystem.Abilities.Components;
+using MyGameplayAbilitySystem.Common.ScriptableObjects;
 
 public class AbilityHotbarManager : MonoBehaviour {
     public ActorAbilitySystem AbilityCharacter;
     public List<AbilityHotbarButton> AbilityButtons;
-    public List<AbilityIconMap> AbilityIconMaps;
+    public AbilityIconMapScriptableObject AbilityIconMaps;
 
     public List<int> AbilityIdentifiers;
 
@@ -18,7 +38,7 @@ public class AbilityHotbarManager : MonoBehaviour {
         World.Active.GetOrCreateSystem<AbilityHotbarUpdateSystem>().AbilityOwnerEntity = AbilityCharacter.AbilityOwnerEntity;
         World.Active.GetOrCreateSystem<AbilityHotbarUpdateSystem>().AbilityButtons = AbilityButtons;
         World.Active.GetOrCreateSystem<AbilityHotbarUpdateSystem>().AbilityIdentifiers = AbilityIdentifiers;
-        World.Active.GetOrCreateSystem<AbilityHotbarUpdateSystem>().AbilityIconMaps = AbilityIconMaps;
+        World.Active.GetOrCreateSystem<AbilityHotbarUpdateSystem>().AbilityIconMaps = AbilityIconMaps.AbilityIconMaps;
 
     }
 
