0d3b933d0b85357a93e1b3e07134a5bee2136b57
sjai013 <sjai013.ece@gmail.com>
2019-11-27 20:43:24 +1000

    ability casting trial

---
+++ Assets/Scripts/PlayerCastActionSystem.cs
@@ -0,0 +1,17 @@
+﻿using MyGameplayAbilitySystem.Abilities;
+using MyGameplayAbilitySystem.AbilitySystem.MonoBehaviours;
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerCastActionSystem : ComponentSystem {
+    protected override void OnUpdate() {
+
+        Entities.WithAll<PlayerCastingControllableTagComponent>().ForEach((Entity entity, ActorAbilitySystem actorAbilitySystem) => {
+            if (Input.GetKeyUp("1")) {
+                // Default attack - trigger cooldown and resource cost
+                (new DefaultAttackAbilityTag()).CreateCooldownEntities(EntityManager, actorAbilitySystem.AbilityOwnerEntity);
+                (new DefaultAttackAbilityTag()).CreateSourceAttributeModifiers(EntityManager, actorAbilitySystem.AbilityOwnerEntity);
+            }
+        });
+    }
+}
