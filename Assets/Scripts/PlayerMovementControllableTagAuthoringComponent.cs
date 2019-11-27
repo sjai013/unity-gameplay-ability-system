f0601d2d9e18d9f67e4acdad27b9cba02c34ce63
sjai013 <sjai013.ece@gmail.com>
2019-11-27 18:20:28 +1000

    Actor models minimalism.  Movement system.

---
+++ Assets/Scripts/PlayerMovementControllableTagAuthoringComponent.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class PlayerMovementControllableTagAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity {
+    // Add fields to your component here. Remember that:
+    //
+    // * The purpose of this class is to store data for authoring purposes - it is not for use while the game is
+    //   running.
+    // 
+    // * Traditional Unity serialization rules apply: fields must be public or marked with [SerializeField], and
+    //   must be one of the supported types.
+    //
+    // For example,
+    //    public float scale;
+
+    public float RotationMultiplier = 5;
+    public float MovementSpeedMultiplier = 5;
+    public struct PlayerMovementControllableTagComponent : IComponentData { }
+    public struct PlayerMovementRotationMultiplierComponent : IComponentData {
+        public float Value;
+    }
+
+    public struct PlayerMovementSpeedMultiplierComponent : IComponentData {
+        public float Value;
+    }
+
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+        // Call methods on 'dstManager' to create runtime components on 'entity' here. Remember that:
+        //
+        // * You can add more than one component to the entity. It's also OK to not add any at all.
+        //
+        // * If you want to create more than one entity from the data in this class, use the 'conversionSystem'
+        //   to do it, instead of adding entities through 'dstManager' directly.
+        //
+        // For example,
+        //   dstManager.AddComponentData(entity, new Unity.Transforms.Scale { Value = scale });
+        dstManager.AddComponentData(entity, new PlayerMovementControllableTagComponent());
+        dstManager.AddComponentData(entity, new PlayerMovementRotationMultiplierComponent
+        {
+            Value = RotationMultiplier
+        });
+
+        dstManager.AddComponentData(entity, new PlayerMovementSpeedMultiplierComponent
+        {
+            Value = MovementSpeedMultiplier
+        });
+
+        dstManager.AddComponentData(entity, new CopyTransformToGameObject());
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovementControllerSystem.cs
@@ -0,0 +1,57 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using static PlayerMovementControllableTagAuthoringComponent;
+
+public class PlayerControllerSystem : JobComponentSystem {
+    // This declares a new kind of job, which is a unit of work to do.
+    // The job is declared as an IJobForEach<Translation, Rotation>,
+    // meaning it will process all entities in the world that have both
+    // Translation and Rotation components. Change it to process the component
+    // types you want.
+    //
+    // The job is also tagged with the BurstCompile attribute, which means
+    // that the Burst compiler will optimize it for the best performance.
+    [BurstCompile]
+    [RequireComponentTag(typeof(PlayerMovementControllableTagComponent))]
+    struct PlayerControllerSystemJob : IJobForEach<Translation, Rotation, PlayerMovementRotationMultiplierComponent, PlayerMovementSpeedMultiplierComponent> {
+        // Add fields here that your job needs to do its work.
+        // For example,
+        //    public float deltaTime;
+
+        public float2 movementVector;
+        public float deltaTime;
+
+        public void Execute(ref Translation translation, ref Rotation rotation, [ReadOnly] ref PlayerMovementRotationMultiplierComponent rotationMultiplierComponent, [ReadOnly] ref PlayerMovementSpeedMultiplierComponent speedMultiplierComponent) {
+            var q = quaternion.AxisAngle(new float3(0.0f, 1.0f, 0.0f), movementVector.x * rotationMultiplierComponent.Value * deltaTime);
+            translation.Value += math.mul(rotation.Value, new float3(0, 0, 1)) * movementVector.y * deltaTime * speedMultiplierComponent.Value;
+            rotation.Value = math.mul(q, rotation.Value);
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies) {
+        float2 movementVector;
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector.y = Input.GetAxis("Vertical");
+        var job = new PlayerControllerSystemJob
+        {
+            deltaTime = Time.deltaTime,
+            movementVector = movementVector
+
+        };
+
+        // Assign values to the fields on your job here, so that it has
+        // everything it needs to do its work when it runs later.
+        // For example,
+        //     job.deltaTime = UnityEngine.Time.deltaTime;
+
+
+
+        // Now that the job is set up, schedule it to be run. 
+        return job.Schedule(this, inputDependencies);
+    }
+}
