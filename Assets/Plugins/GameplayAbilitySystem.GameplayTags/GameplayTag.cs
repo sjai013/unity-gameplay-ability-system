cd95c3e16e1ac9c4f8acf37c299991bd55e94c1f
Sahil Jain <sjai013.ece@gmail.com>
2020-10-22 16:13:17 +1000

    minor mods

--- Assets/Plugins/GameplayAbilitySystem.GameplayTags/GameplayTag.cs
+++ Assets/Plugins/GameplayAbilitySystem.GameplayTags/GameplayTag.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Unity.Mathematics;
 
 namespace GamplayAbilitySystem.GameplayTags
 {
@@ -25,17 +26,17 @@ namespace GamplayAbilitySystem.GameplayTags
 
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool IsEqualTo(GameplayTag other)
+        public bool Equals(GameplayTag other)
         {
-            // Apply bitmask based on this
-            // e.g. for A.B.C.D, this A matches other A.*
             var mask = 0xFFFFFFFF;
             mask &= L3 == 0 ? 0xFFFFFF00 : 0xFFFFFFFF;
             mask &= L2 == 0 ? 0xFFFF0000 : 0xFFFFFFFF;
             mask &= L1 == 0 ? 0xFF000000 : 0xFFFFFFFF;
             mask &= L0 == 0 ? 0x00000000 : 0xFFFFFFFF;
             return (mask & other.Id) == Id;
+
         }
+
     }
 
 }
