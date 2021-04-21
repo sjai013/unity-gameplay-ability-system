9f66dd9daaeda5e994f22573b3e63538ad9b23cf
Sahil Jain <sjai013.ece@gmail.com>
2021-04-21 15:55:25 +1000

    add attribute change log event handler

---
+++ Assets/LogAttributeChangeEventHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using AttributeSystem.Authoring;
+using AttributeSystem.Components;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gameplay Ability System/Attribute Event Handler/Attribute Change Log")]
+public class LogAttributeChangeEventHandler : AbstractAttributeEventHandler
+{
+
+    [SerializeField]
+    private AttributeScriptableObject PrimaryAttribute;
+
+    public override void PreAttributeChange(AttributeSystemComponent attributeSystem, List<AttributeValue> prevAttributeValues, ref List<AttributeValue> currentAttributeValues)
+    {
+
+        var attributeCacheDict = attributeSystem.mAttributeIndexCache;
+        if (attributeCacheDict.TryGetValue(PrimaryAttribute, out var primaryAttributeIndex))
+        {
+            var prevValue = prevAttributeValues[primaryAttributeIndex].CurrentValue;
+            var currentValue = currentAttributeValues[primaryAttributeIndex].CurrentValue;
+
+            if (prevValue != currentValue)
+            {
+                Debug.Log($"{attributeSystem.gameObject.name}: {currentAttributeValues[primaryAttributeIndex].Attribute.Name} modified.  Old Value: {prevValue}.  New Value: {currentValue}.");
+            }
+        }
+
+    }
+
+
+}
