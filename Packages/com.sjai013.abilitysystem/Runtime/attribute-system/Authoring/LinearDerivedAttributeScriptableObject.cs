f66e22606b86069aae4987b6f5df876c23623215
Sahil Jain <sjai013.ece@gmail.com>
2021-10-03 06:57:45 +1000

    Add caching to linear derived attribute to improve performance

--- Packages/com.sjai013.abilitysystem/Runtime/attribute-system/Authoring/LinearDerivedAttributeScriptableObject.cs
+++ Packages/com.sjai013.abilitysystem/Runtime/attribute-system/Authoring/LinearDerivedAttributeScriptableObject.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using AttributeSystem.Authoring;
 using AttributeSystem.Components;
 using UnityEngine;
 
@@ -12,13 +10,32 @@ namespace AttributeSystem.Authoring
         [SerializeField] private AttributeScriptableObject m_BaseAttribute;
         [SerializeField] private AnimationCurve m_MappingCurve;
 
+        private int m_AttributeIndexCache = -1;
+
         public override AttributeValue CalculateAttributeValue(AttributeValue attributeValue, List<AttributeValue> otherAttributeValues)
         {
             // Find desired attribute in list
-            AttributeValue baseAttributeValue = otherAttributeValues.Find(x => x.Attribute == m_BaseAttribute);
+            AttributeValue? baseAttributeValue = null;
+
+            if (!(m_AttributeIndexCache >= 0
+                && m_AttributeIndexCache < otherAttributeValues.Count - 1
+                && otherAttributeValues[m_AttributeIndexCache].Attribute == m_BaseAttribute))
+            {
+                m_AttributeIndexCache = FindAttributeIndexInList(attributeValue, otherAttributeValues);
+            }
+
+            if (m_AttributeIndexCache < 0)
+            {
+                Debug.LogWarning("Attempt to reference non-existent attribute.  Ignoring.");
+                m_AttributeIndexCache = -1;
+                return attributeValue;
+            }
+            
+            baseAttributeValue = otherAttributeValues[m_AttributeIndexCache];
+
 
             // Calculate new base value, using the base attribute value and mapping curve
-            attributeValue.BaseValue = m_MappingCurve.Evaluate(baseAttributeValue.CurrentValue);
+            attributeValue.BaseValue = m_MappingCurve.Evaluate(baseAttributeValue.Value.CurrentValue);
 
             // Calculate the current value
             attributeValue.CurrentValue = (attributeValue.BaseValue + attributeValue.Modifier.Add) * (attributeValue.Modifier.Multiply + 1);
@@ -28,6 +45,19 @@ namespace AttributeSystem.Authoring
                 attributeValue.CurrentValue = attributeValue.Modifier.Override;
             }
             return attributeValue;
+        }
+
+        public int FindAttributeIndexInList(AttributeValue attributeValue, List<AttributeValue> otherAttributeValues)
+        {
+            for (var i = 0; i < otherAttributeValues.Count; i++)
+            {
+                if (otherAttributeValues[i].Attribute == m_BaseAttribute)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
